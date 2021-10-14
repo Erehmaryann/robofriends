5370149ecbf2ce8325dd26534cac415900af8f93
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-14 15:35:02 -0700

    set states for robots and input field

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-    );
+export class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        };
+    }
+    render() {
+        return (
+
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox />
+                <CardList robots={this.state.robots} />
+            </div>
+        );
+    }
 };
 
 export default App;
