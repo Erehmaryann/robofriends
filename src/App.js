f7195ff0e430b97af720af9c3cede095b81051e1
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-14 15:22:51 -0700

    finish creating app UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = () => {
     return (
-        <div>
+        <div className="tc">
             <h1>RoboFriends</h1>
+            <SearchBox />
             <CardList robots={robots} />
         </div>
     );
---
+++ src/SearchBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div className="pa2">
+            <input type="search"
+                className="pa3 ba b--green bg-lightest-blue"
+                placeholder="search robots" />
+        </div>
+    );
+};
+
+export default SearchBox;
