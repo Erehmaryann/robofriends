e50572ff138066799cea2d0b672565271e56b0f0
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-14 14:56:03 -0700

    created card list component

---
+++ src/CardList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    return (
+        <div>
+            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+        </div>
+    );
+};
+
+export default CardList;
