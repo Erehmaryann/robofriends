03ba4d0c91777fb7836bc76ced6d32030786bbec
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-15 13:37:09 -0700

    nice folder structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
 export class App extends Component {
@@ -26,11 +26,12 @@ export class App extends Component {
     };
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
-        if (this.state.robots.length === 0) {
+        if (robots.length === 0) {
             return <h1 className="tc">Loading...</h1>;
         } else {
             return (
