1b0f6d58221b16b04a07bb920b9916b738de1d05
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-16 14:25:58 -0700

    added error boundary component

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+    if (true) {
+        throw new Error('NO!');
+    }
+
     return (
         <div>
             {robots.map((user, idx) => {
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
 export class App extends Component {
@@ -38,7 +39,9 @@ export class App extends Component {
                     <h1 className="f1">RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange} />
                     <Scroll>
-                        <CardList robots={filteredRobots} />
+                        <ErrorBoundary>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundary>
                     </Scroll>
                 </div>
             );
