56e5de14a64e19c4160d5c677a7a247bd16bd514
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-22 13:08:21 -0700

    apply middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots } from './reducers';
 import 'tachyons';
 import './index.css';
 
-
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
