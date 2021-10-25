cbf62b5012617537d0df8cd8a4f49f12fe574b35
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-25 11:24:41 -0700

    finish setting up redux thunk

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots } from './reducers';
@@ -10,7 +11,8 @@ import 'tachyons';
 import './index.css';
 
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store =
+  createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers.js
+++ src/reducers.js
@@ -1,4 +1,11 @@
-import { CHANGE_SEARCHFIELD } from './constants';
+/* eslint-disable default-case */
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+}
+    from './constants';
 
 const initialState = {
     searchField: ''
@@ -6,9 +13,20 @@ const initialState = {
 
 export const searchRobots = (state = initialState, action = {}) => {
     switch (action.type) {
-        case CHANGE_SEARCHFIELD:
+        case CHANGE_SEARCH_FIELD:
             return /*Object.assign({}, state, { searchField: action.payload });*/ { ...state, searchField: action.payload };
         default:
             return state;
+    }
+};
+
+export const requestRobots = (state = initialState, action = {}) => {
+    switch (action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true });
+        case REQUEST_ROBOTS_SUCCESS:
+            return { ...state, robots: action.payload, isPending: false };
+        case REQUEST_ROBOTS_FAILED:
+            return Object.assign({}, state, { error: action.payload, isPending: false });
     }
 };
