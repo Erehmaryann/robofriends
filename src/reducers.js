11a1e13d206a654672c505ecc48806db4d695879
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-25 11:41:46 -0700

    robots reducer setup

--- src/reducers.js
+++ src/reducers.js
@@ -7,11 +7,11 @@ import {
 }
     from './constants';
 
-const initialState = {
+const initialStateSearch = {
     searchField: ''
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
     switch (action.type) {
         case CHANGE_SEARCH_FIELD:
             return /*Object.assign({}, state, { searchField: action.payload });*/ { ...state, searchField: action.payload };
@@ -20,7 +20,13 @@ export const searchRobots = (state = initialState, action = {}) => {
     }
 };
 
-export const requestRobots = (state = initialState, action = {}) => {
+const initialStateRobots = {
+    isPending: false,
+    robots: [],
+    error: ''
+};
+
+export const requestRobots = (state = initialStateRobots, action = {}) => {
     switch (action.type) {
         case REQUEST_ROBOTS_PENDING:
             return Object.assign({}, state, { isPending: true });
@@ -28,5 +34,7 @@ export const requestRobots = (state = initialState, action = {}) => {
             return { ...state, robots: action.payload, isPending: false };
         case REQUEST_ROBOTS_FAILED:
             return Object.assign({}, state, { error: action.payload, isPending: false });
+        default:
+            return state;
     }
 };
